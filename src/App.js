a9d2c49c2d941013f7d493899296a713f93629a5
Duc Ho <hotienduc05012002@gmail.com>
2022-08-02 21:44:54 +0700

    Added React

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./components/Card";
+import data from "./data/mockup";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Study React
+        </a>
+      </header>
+      <main class="App-body">
+        {data.map((element) => {
+          return (
+            <Card title={element.title} content={element.content}>
+              <div>Likes</div>
+            </Card>
+          );
+        })}
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import cardStyles from "../styles/Card.module.scss";
+import ReactionButton from "./ReactionButton";
+import { useState, useEffect } from "react";
+import logo from "../logo.svg";
+import {
+  faThumbsUp,
+  faThumbsDown,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Card = (props) => {
+  const [likes, setLikes] = useState(0);
+  const [imgUrl, setImgUrl] = useState("");
+  useEffect(() => {
+    const unsplashRandom = async () => {
+      const response = await fetch(
+        "https://api.unsplash.com/photos/random/?client_id=4hi7fgNG_TmAkLHRrx1b29uPSzSkvrPuwaVi60zr72E"
+      );
+      return response;
+    };
+    unsplashRandom().then((data) => console.log(data));
+  }, []);
+  return (
+    <div className={cardStyles.card}>
+      <h2 className={cardStyles.cardHeading}>{props.title}</h2>
+      <p className={cardStyles.cardBody}>
+        {props.content}, ipsum dolor sit amet consectetur adipisicing
+        elit. Doloribus id itaque ad ducimus laborum, hic repudiandae
+        harum sapiente beatae odit facere, numquam nobis odio. Impedit
+        saepe repudiandae iste illum officia!
+      </p>
+      {props.children}
+      <img src={logo} alt="Icon"></img>
+      {likes}
+      <ReactionButton
+        icon={faThumbsUp}
+        onClick={() => setLikes(likes + 1)}
+      />
+      <ReactionButton
+        icon={faThumbsDown}
+        onClick={() => setLikes(likes - 1)}
+      />
+    </div>
+  );
+};
+
+export default Card;
